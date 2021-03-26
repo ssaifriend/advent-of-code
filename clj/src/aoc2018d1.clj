fb65bdb12a96e61b018e4fe6a1e95c4316b9c93b
Park Jinseong <js.park@greenlabs.co.kr>
2021-03-26 16:06:21 +0900

    Add 2018.1 - clojure

--- clj/src/aoc2018d1.clj
+++ clj/src/aoc2018d1.clj
@@ -1,1 +1,45 @@
-(ns aoc2018d1)
+(ns aoc2018d1
+  (:require [clojure.string :as s]))
+
+(defn parse->process
+  "line 단위로 분리하고 숫자로 변환합니다."
+  [ds]
+  (->> (s/split ds #"\n")
+       (map #(Integer/parseInt %))))
+
+(defn ->aggregate-part-1
+  "입력받은 값을 모두 더합니다."
+  [ds]
+  (apply + ds))
+
+(defn ->find-twice-search
+  "더할때 반환값이 두번째로 나오는 값을 반환합니다."
+  [ds]
+  (loop [sum 0
+         sums #{}
+         ds (cycle ds)]
+    (let [sum (+ sum (first ds))]
+      (if (contains? sums sum)
+        sum
+        (recur sum
+               (conj sums sum)
+               (rest ds))))))
+
+(comment
+  (->>
+    (slurp "../input/2018/2018.1.input")
+    (parse->process)
+    (->aggregate-part-1)
+    (println))
+  (->>
+    (slurp "../input/2018/2018.1.input")
+    (parse->process)
+    (->find-twice-search)
+    (println))
+
+  (let [test #{1 2 3}]
+    (contains? test 3))
+  (let [test (cycle [1 2 3])]
+    (first test))
+  (let [test #{}]
+    (contains? test 0)))
