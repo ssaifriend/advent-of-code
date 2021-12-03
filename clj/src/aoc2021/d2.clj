81ccfe16f91f990c1cceeb81a47156d788463a41
Park Jinseong <js.park@greenlabs.co.kr>
2021-12-03 15:18:35 +0900

    2021.2 part1

---
+++ clj/src/aoc2021/d2.clj
@@ -0,0 +1,27 @@
+(ns aoc2021.d2
+  (:require [util :refer [read-file]]
+            [clojure.string :as s]))
+
+(defn get-read-file []
+  (-> "2021/2021.2.input" read-file s/split-lines))
+
+(defn parse [inputs]
+  (map (fn [input]
+         (let [[action x] (s/split input #" ")]
+           {:action (keyword action)
+            :x (Integer/parseInt x)}))
+       inputs))
+
+(defn move [inputs]
+  (reduce (fn [m {:keys [action x]}]
+            (case action
+              :forward (update m :position #(+ % x))
+              :up (update m :depth #(+ % x))
+              :down (update m :depth #(- % x))))
+          {:depth 0
+           :position 0}
+          inputs))
+
+(comment
+  (let [result (-> (get-read-file) parse move)]
+    (* (:depth result) (:position result))))
