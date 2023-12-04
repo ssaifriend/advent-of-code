811506e3f319e8478abf1bd3b2ba02cc22d1286e
Jinseong Park <jinseong.park@greenlabs.co.kr>
2023-12-04 16:59:46 +0900

    clojure 2023 d1 p2

--- clj/src/aoc2023/d1.clj
+++ clj/src/aoc2023/d1.clj
@@ -11,15 +11,33 @@
 (defn string-int->int [c]
   (- (int c) (int \0)))
 
+(defn filter-string-int [s]
+  (filter string-int? (seq s)))
+
+(defn ->make-digit [s]
+  (let [a (first s)
+        b (last s)]
+    (+ (* 10 (string-int->int a))
+       (string-int->int b))))
+
 (defn q1 [ss]
   (->> ss
-       (map (fn [row] (->> (seq row)
-                           (filter string-int?))))
-       (map (fn [row]
-              (let [a (first row)
-                    b (last row)]
-                (+ (* 10 (string-int->int a))
-                   (string-int->int b)))))
+       (map filter-string-int)
+       (map ->make-digit)
+       (apply +)))
+
+(defn text-int->int [s]
+  (let [pattern "(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)"
+        text-int->int-map {"one" \1 "two" \2 "three" \3 "four" \4 "five" \5 "six" \6 "seven" \7 "eight" \8 "nine" \9
+                           "1" \1 "2" \2 "3" \3 "4" \4 "5" \5 "6" \6 "7" \7 "8" \8 "9" \9}]
+    [(text-int->int-map (first (re-find (re-pattern pattern) s)))
+     (text-int->int-map (second (re-find (re-pattern (str ".*(?<=" pattern ")")) s)))]))
+
+(defn q2 [ss]
+  (->> ss
+       (map text-int->int)
+       (map filter-string-int)
+       (map ->make-digit)
        (apply +)))
 
 (comment
@@ -31,4 +49,15 @@ treb7uchet")
   (string-int? \a)
   (q1 (s/split-lines sample))
   (q1 (read-file->))
+
+  (def sample "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen")
+  (map text-int->int (s/split-lines sample))
+  (q2 (s/split-lines sample))
+  (q2 (read-file->))
   :rcf)
